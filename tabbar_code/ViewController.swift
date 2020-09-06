4feaa9d3c3c7675f9a4f6d3ddb0edf9bf31b015d
shinohara-san <someiyoshino0809@gmail.com>
2020-09-06 20:32:18 +0900

    complete

--- tabbar_code/ViewController.swift
+++ tabbar_code/ViewController.swift
@@ -9,12 +9,98 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let button: UIButton = {
+       let button = UIButton()
+        button.setTitle("Log in", for: .normal)
+        button.backgroundColor = .systemBackground
+        button.setTitleColor(.label, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemIndigo
+        view.addSubview(button)
+        button.addTarget(self,
+                         action: #selector(didTapButton),
+                         for: .touchUpInside)
+    }
+    
+    @objc private func didTapButton(){
+        let tabbarVC = UITabBarController()
+        
+//        let vc1 = FirstViewController()
+        let vc1 = UINavigationController(rootViewController: FirstViewController())
+        let vc2 = UINavigationController(rootViewController: SecondViewController())
+        let vc3 = UINavigationController(rootViewController: ThirdViewController())
+        let vc4 = UINavigationController(rootViewController: FourthViewController())
+        let vc5 = UINavigationController(rootViewController: FifthViewController())
+        
+        vc1.title = "Home"
+        vc2.title = "Contact"
+        vc3.title = "Help"
+        vc4.title = "About"
+        vc5.title = "Settings"
+        
+        tabbarVC.setViewControllers([vc1, vc2, vc3, vc4, vc5], animated: false)
+        
+        guard let items = tabbarVC.tabBar.items else {return}
+        
+        let images = ["house", "bell", "person.circle", "star", "gear"]
+        
+        for x in 0..<images.count {
+            items[x].image = UIImage(systemName: images[x])
+        }
+        
+        tabbarVC.modalPresentationStyle = .fullScreen
+        present(tabbarVC, animated: true)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.frame = CGRect(x: 0, y: 0, width: view.frame.width/3, height: view.frame.height/12)
+        button.center = view.center
     }
 
 
 }
+
+class FirstViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+//        ここでtitleを設定しても、viewDidloadはviewがloadされないと
+//        つまりタブがタップされて表示されないとtitleが表示されない
+    }
+}
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
+class ThirdViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
+
+class FourthViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
+
+class FifthViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
+
 
